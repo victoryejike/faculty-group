7fb3e200b50e2ef680de1e6c2438e76599332ef6
Ejike Victory <victoryejike@gmail.com>
2021-11-11 05:31:10 +0100

    feat: created card, header, button component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-3 gap-4">
+      <div>1</div>
+      <div>2</div>
+      <div>2</div>
+      <div>2</div>
+      <div>2</div>
+      <div>2</div>
     </div>
   );
 }
